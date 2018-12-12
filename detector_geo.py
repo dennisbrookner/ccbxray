7aaa4f2fec43f4c363c9707d1fb070adc68f5da4
Kevin Dalton <kevinmdalton@gmail.com>
2018-12-12 17:21:57 -0500

    confirmed consistency between python and xds 	modified:   detector_geo.py         -- added many methods to swing_diffractometer. i have also         cleared that the geometry is consistent with XDS pix2lab         program. can write compatible headers for arbitrary swing angles         to an XDS.INP file

--- detector_geo.py
+++ detector_geo.py
@@ -34,7 +34,7 @@ class swing_diffractometer():
         self.NX = NX
         self.NY = NY
         self.CHI = CHI
-        self.X_AXIS = np.array([-np.cos(np.deg2rad(self.CHI)), 0, -np.sin(np.deg2rad(self.CHI))])
+        self.X_AXIS = np.array([np.cos(np.deg2rad(self.CHI)), 0, np.sin(np.deg2rad(self.CHI))])
         self.Y_AXIS = np.array([0, 1, 0])
         self.DETECTOR_ORG = np.array([-self.F*np.sin(np.deg2rad(self.CHI)),0,self.F*np.cos(np.deg2rad(self.CHI))])
 
@@ -43,14 +43,15 @@ class swing_diffractometer():
             stride = 10
         from mpl_toolkits.mplot3d import Axes3D
         from matplotlib import pyplot as plt
-        x,y = np.meshgrid(np.arange(self.NX) + 1, np.arange(self.NY) +1)
-        x = np.concatenate((np.arange(self.NX) +1, self.NX*np.ones(self.NY), np.arange(self.NX)[::-1] +1, np.ones(self.NY) ))
-        y = np.concatenate((np.zeros(self.NX), np.arange(self.NY) + 1, self.NY*np.ones(self.NX), np.arange(self.NY)[::-1] +1))
-        x,y = x.flatten(),y.flatten()
-        x,y,z = self(x, y).T
         f = plt.figure()
         ax = f.add_subplot(111, projection='3d')
+        x,y,z = self.panel_edge_xyz(stride).T
         ax.scatter(x[::stride], y[::stride], z[::stride])
+
+        x,y,z = self.pix2lab(*self.panel_edge_pixels(stride)).T
+        ax.plot(x[::stride], y[::stride], z[::stride], c='goldenrod')
+
+
         x,y,z = self.DETECTOR_ORG[:,None] * np.linspace(0, 1, 100)
         ax.scatter(x[::stride], y[::stride], z[::stride], c='r')
         ax.scatter(0, 0, 0, c='k')
@@ -62,6 +63,90 @@ class swing_diffractometer():
         ax.set_ylabel("Y (mm)")
         ax.set_zlabel("Z (mm)")
         plt.show()
+
+    def panel_edge_pixels(self, stride=None):
+        """
+        Return the indices of the pixels at the edge of the detector in the lab frame. 
+        PARAMTERS
+        ---------
+        stride : int (optional)
+            Subsample the detector edge by only reporting every stride'th pixel.
+        RETURNS
+        -------
+        x : np.ndarray
+            The x-indices of the detector edge pixels. 
+        y : np.ndarray
+            The y-indices of the detector edge pixels. 
+        """
+        if stride is None:
+            stride = 1
+        x,y = np.meshgrid(np.arange(self.NX) + 1, np.arange(self.NY) +1)
+        x = np.concatenate((np.arange(self.NX) +1, self.NX*np.ones(self.NY), np.arange(self.NX)[::-1] +1, np.ones(self.NY) ))
+        y = np.concatenate((np.zeros(self.NX), np.arange(self.NY) + 1, self.NY*np.ones(self.NX), np.arange(self.NY)[::-1] +1))
+        x,y = x.flatten(),y.flatten()
+        return x,y
+
+
+    def panel_edge_xyz(self, stride=None):
+        """
+        Return the coordinates of the pixels at the edge of the detector in the lab frame. 
+        PARAMTERS
+        ---------
+        stride : int (optional)
+            Subsample the detector edge by only reporting every stride'th pixel.
+        RETURNS
+        -------
+        coordinates : np.ndarray
+            The coordinates of the detector edge pixels. coordinates.shape = (number of pixels, 3)
+
+        """
+        if stride is None:
+            stride = 1
+        x,y = np.meshgrid(np.arange(self.NX) + 1, np.arange(self.NY) +1)
+        x = np.concatenate((np.arange(self.NX) +1, self.NX*np.ones(self.NY), np.arange(self.NX)[::-1] +1, np.ones(self.NY) ))
+        y = np.concatenate((np.zeros(self.NX), np.arange(self.NY) + 1, self.NY*np.ones(self.NX), np.arange(self.NY)[::-1] +1))
+        x,y = x.flatten(),y.flatten()
+        return self(x, y)
+
+    def generate_xds_in(self):
+        text = ""
+        text = text + "ORGX= {} ".format(self.NX / 2)
+        text = text + "ORGY= {} \n".format(self.NY / 2)
+        text = text + "DETECTOR_DISTANCE= {}\n".format(self.F)
+        text = text + "NX= {} ".format(self.NY)
+        text = text + "NY= {} ".format(self.NY)
+        text = text + "QX= {} ".format(self.QY)
+        text = text + "QY= {}\n".format(self.QY)
+        text = text + "DIRECTION_OF_DETECTOR_X-AXIS= {} {} {}\n".format(*self.X_AXIS)
+        text = text + "DIRECTION_OF_DETECTOR_Y-AXIS= {} {} {}\n".format(*self.Y_AXIS)
+        text = text + "INCIDENT_BEAM_DIRECTION= 0 0 1\n"
+        return text
+
+    def pix2lab(self, x, y):
+        """
+        Use the pix2lab program from XDS to compute the lab frame coordinates for detector pixels. 
+        PARAMTERS
+        ---------
+        x : numerical or array-like
+            An integer or array of integers corresponding to pixel x-inidces. Non-integers will be coerced to int. 
+        y : numerical or array-like
+            An integer or array of integers corresponding to pixel y-indices. Non-integers will be coerced to int. 
+
+        RETURNS
+        -------
+        r : np.ndarray
+            Array of lab frame coordinates for the detector pixels. These will be computed by the pix2lab program.
+        """
+        xdsin = self.generate_xds_in()
+        with open('XDS.INP', 'w') as out:
+            out.write(xdsin)
+        from subprocess import Popen,PIPE
+        p = Popen('pix2lab', stdin=PIPE, stdout=PIPE, encoding='utf-8')
+        pix = '\n'.join(('{} {} 1'.format(i,j) for i,j in zip(x,y)))
+        pix += '1 1 -1' #This kills the pix2lab session
+        stdout,stderr = p.communicate(pix)
+        pos = np.array([i.split()[-3:] for i in stdout.split('\n')[3::4]], dtype=float)
+        return pos
 
     def __call__(self, x, y):
         """
